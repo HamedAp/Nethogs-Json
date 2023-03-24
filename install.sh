5146110751e7c0fe16e9a1e3c291f2f6d6aaedeb
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-03-24 23:34:52 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #By Hamed Ap
+apt update -y
+apt upgrade -y
+apt install make
 
 sudo wget -O /root/nethogs.zip https://github.com/HamedAp/Nethogs-Json/archive/refs/heads/main.zip
 
 unzip /root/nethogs.zip
 
-mv /root/Nethogs-Json-main /root/nethogs
+mv -f /root/Nethogs-Json-main /root/nethogs
 
 
 cd /root/nethogs/
