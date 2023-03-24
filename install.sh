690fd4262a967ac947c301970de499aff72931ad
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-03-24 23:58:46 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #By Hamed Ap
 apt update -y
 apt upgrade -y
-apt-get install build-essential libncurses5-dev libpcap-dev make
+apt-get install build-essential libncurses5-dev libpcap-dev make -y
 
 sudo wget -O /root/nethogs.zip https://github.com/HamedAp/Nethogs-Json/archive/refs/heads/main.zip
 
