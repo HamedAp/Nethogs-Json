b8d40e216ae6cef19db8608499e49db1099ce705
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-10-16 02:01:17 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 #By Hamed Ap
 
 if command -v apt-get >/dev/null; then
-apt update -y
-apt-get install build-essential libncurses5-dev libpcap-dev make zip unzip wget -y
+sudo apt update -y
+sudo apt-get install build-essential libncurses5-dev libpcap-dev make zip unzip wget -y
 elif command -v yum >/dev/null; then
 yum update -y
 yum install gcc-c++ libpcap-devel.x86_64 libpcap.x86_64 "ncurses*"
 fi
 sudo wget -O /root/nethogs.zip https://github.com/HamedAp/Nethogs-Json/archive/refs/heads/main.zip
-unzip /root/nethogs.zip
-mv -f /root/Nethogs-Json-main /root/nethogs
-cd /root/nethogs/
-chmod 744 /root/nethogs/determineVersion.sh
+sudo unzip /root/nethogs.zip
+sudo mv -f /root/Nethogs-Json-main /root/nethogs
+sudo cd /root/nethogs/
+sudo chmod 744 /root/nethogs/determineVersion.sh
 sudo make install
-hash -r
-cp /usr/local/sbin/nethogs /usr/sbin/nethogs -f
-rm -fr /root/nethogs /root/nethogs.zip
+sudo hash -r
+sudo cp /usr/local/sbin/nethogs /usr/sbin/nethogs -f
+sudo rm -fr /root/nethogs /root/nethogs.zip
 sudo setcap "cap_net_admin,cap_net_raw,cap_dac_read_search,cap_sys_ptrace+pe" /usr/local/sbin/nethogs
