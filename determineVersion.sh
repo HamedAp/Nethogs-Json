083dae2569631bfbea37a917eb79d4435c1591fe
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2026-05-29 20:48:08 +0330

    Update determineVersion.sh

--- determineVersion.sh
+++ determineVersion.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ $(git rev-parse --show-toplevel 2>/dev/null) == $(pwd) ]; then
-  git describe --tags 2>/dev/null | tail -c +2
-else
-  pwd | sed -e "s/.*\\///" | sed -e "s/[^-]*//" | tail -c +2
-fi
+echo "0.8.8"
